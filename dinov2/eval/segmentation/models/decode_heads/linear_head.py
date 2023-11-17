502c1bb9b666baa4a0c002abbd51835daf81cfc6
Evan C <evdcush@protonmail.com>
2023-11-18 07:41:01 +0900

    Updated mmlab dep usage to mmlab-2.0 for seg programs

--- dinov2/eval/segmentation/models/decode_heads/linear_head.py
+++ dinov2/eval/segmentation/models/decode_heads/linear_head.py
@@ -8,7 +8,8 @@ import torch.nn as nn
 
 from mmseg.models.builder import HEADS
 from mmseg.models.decode_heads.decode_head import BaseDecodeHead
-from mmseg.ops import resize
+#from mmseg.ops import resize
+from mmseg.models.utils.wrappers import resize
 
 
 @HEADS.register_module()
